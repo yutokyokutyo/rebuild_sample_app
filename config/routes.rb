bfbbb3ef9b181965933fe0bb262f5310e7962c1d
y.kondo <k.yuto.cre@gmail.com>
2017-03-26 18:56:47 +0900

    make Contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -27,4 +27,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response:success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response:success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
