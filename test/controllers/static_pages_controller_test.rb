d3dcf741f6f41066fc7b030a56d38df386072434
y.kondo <k.yuto.cre@gmail.com>
2017-03-26 16:59:59 +0900

    root_url に home をルーティング

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
