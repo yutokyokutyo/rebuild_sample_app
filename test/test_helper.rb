e2a7b57f19c293c7555e23a9c8ae3836c1fcbb0f
y.kondo <y.kondo@pepabo.com>
2017-03-30 12:24:09 +0900

    5.3.4 リンクのテスト

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,6 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
