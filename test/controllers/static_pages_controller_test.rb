90e01d6b4aeec58f3288ca25046d7cfe9e18802d
y.kondo <y.kondo@pepabo.com>
2017-03-30 11:27:57 +0900

    5.3.2 RailsのルートURL

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,36 +1,32 @@
 require 'test_helper'
 
+def setup
+  @base_title = "Ruby on Rails Tutorial Sample App"
+end
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
-    assert_response:success
+    get about_path
+    assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
-    assert_response:success
+    get contact_path
+    assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
 end
