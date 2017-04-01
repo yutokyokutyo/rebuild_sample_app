be347d88c41657006055859d6caea7510091f185
y.kondo <k.yuto.cre@gmail.com>
2017-04-01 11:44:10 +0900

    長さの検証

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,14 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "   "
     assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
