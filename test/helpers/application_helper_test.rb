e6b3c60d29167b680d2717418038320d03cafc34
y.kondo <k.yuto.cre@gmail.com>
2017-04-01 19:59:50 +0900

    assert_equal は第一引数に期待する値を書くべきだと思う。
    rails tutorial がおかしい気がしている

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-      assert_equal full_title, 'Ruby on Rails Tutorial Sample App'
-      assert_equal full_title("Help"), 'Help | Ruby on Rails Tutorial Sample App'
+      assert_equal 'Ruby on Rails Tutorial Sample App', full_title
+      assert_equal 'Help | Ruby on Rails Tutorial Sample App', full_title("Help")
   end
 end
