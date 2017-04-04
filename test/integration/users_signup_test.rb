8fd6b7762d8987af1ce207909b8525688419f28e
y.kondo <y.kondo@pepabo.com>
2017-04-04 14:20:20 +0900

    ユーザー登録時のエラーメッセージについてテスト追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
   end
 end
