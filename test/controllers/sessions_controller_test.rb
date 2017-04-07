6d5310cd827e71a4829ca1068c082d567a0a47e8
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 00:27:41 +0900

    Sessionsコントローラのテストで名前付きルートを使うようにする

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get sessions_new_url
+    get login_path
     assert_response :success
   end
 
