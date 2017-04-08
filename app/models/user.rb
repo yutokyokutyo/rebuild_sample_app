08863362c76d2486c8dc1038430e536397f61f66
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 16:30:08 +0900

    トークン生成用メソッドを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,9 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  #ランダムトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
