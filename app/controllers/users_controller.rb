6a150f571a6e09157e098fef90a7d772bfd1ae6a
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 15:08:03 +0900

    ユーザー登録中にログインする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user) #user_url(@user)
     else
