df3296951a6573b8e828a1a4c00a52adf3c8e5e1
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 13:29:12 +0900

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
