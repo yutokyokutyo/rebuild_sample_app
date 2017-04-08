ae27e2ebc777c92a71a05664b0575096fb34eb6b
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 17:17:30 +0900

    永続セッションのためにユーザーをDBに記憶する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence:true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -15,8 +16,14 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  #ランダムトークンを返す
+  # ランダムトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをDBに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
