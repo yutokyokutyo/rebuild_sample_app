5d0843e7a357f754a0d486cfcbe50806ced3dd83
y.kondo <k.yuto.cre@gmail.com>
2017-04-09 00:17:01 +0900

    永続的セッションのcurrent_userを更新する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,9 +12,17 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  #現在ログイン中のユーザーを返す(いる場合)
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   #ユーザーがログインしていればtrue、その他ならfalseを返す
