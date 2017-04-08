2ff52e0badf62f86f94fbf5c6384ebc9dd1e2066
y.kondo <k.yuto.cre@gmail.com>
2017-04-09 00:15:17 +0900

    ユーザーを記録させる

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーを永続的セッションに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   #現在ログイン中のユーザーを返す(いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
