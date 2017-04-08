c8a7cf4270244b4b211cbb2e277bd4fc0db57e1c
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 10:45:00 +0900

    再リクエストが送られた際にエラーメッセージを残留させないようにする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
