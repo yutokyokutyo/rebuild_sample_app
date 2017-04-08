a3316a0c02c025d3deb7e0ee6106a8d511ecd8bf
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 15:13:19 +0900

    テスト中のログインステータスを論理値で返すメソッドを追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,9 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
+
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
