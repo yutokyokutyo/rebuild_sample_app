01eb2be55f188047739934b9aa394e80746aa02a
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 13:15:54 +0900

    logged_in?ヘルパーメソッドを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,4 +9,9 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  #ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
