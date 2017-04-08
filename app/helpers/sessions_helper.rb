0742c718e3c4a1f5a5b70f9c66a9d869fed95950
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 13:02:09 +0900

    セッションに含まれる現在のユーザーを検索する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,9 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  #現在ログイン中のユーザーを返す(いる場合)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
