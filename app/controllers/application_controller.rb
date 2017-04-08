170ca18e1ed28bbe35e42587ad5f1f2fd9962663
y.kondo <k.yuto.cre@gmail.com>
2017-04-08 11:08:08 +0900

    ApplicationコントローラーにSessionヘルパーモジュールをインクルード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-      render html: "hello,Chicorita!"
-  end
+  include SessionHelper
 end
