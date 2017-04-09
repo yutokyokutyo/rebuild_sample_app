de8e6ea847f9061dd65c5643714d3c0a98a210a0
y.kondo <k.yuto.cre@gmail.com>
2017-04-09 17:08:21 +0900

    ダイジェストが存在いない場合のauthenticated?メソッドのテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ApplicationRecord
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,4 +72,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "auhthenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
