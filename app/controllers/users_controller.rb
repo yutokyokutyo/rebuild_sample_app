d4a0ca9c923869423ae50672f3e5ab3108aea7b3
y.kondo <y.kondo@pepabo.com>
2017-04-12 12:46:12 +0900

    ユーザー認可機能を追加
    ログインユーザーでないと編集・更新できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -38,5 +40,15 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済みユーザーがどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
     end
 end
--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -6,6 +6,7 @@ class UserEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name:  "",
@@ -18,6 +19,7 @@ class UserEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
