00a30486ea8e660e2f850ff4b750a7c9aaf8a09c
y.kondo <y.kondo@pepabo.com>
2017-04-12 11:42:47 +0900

    ユーザー編集に成功するテストを追加

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -16,4 +16,20 @@ class UserEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select 'div.alert', 'The form contains 4 errors.'
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
