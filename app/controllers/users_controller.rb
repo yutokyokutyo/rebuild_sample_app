11108ff5096f83ad6ad738e73b1aabc5453b5700
y.kondo <y.kondo@pepabo.com>
2017-04-14 11:05:38 +0900

    フレンドリーフォワーディングを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
     # ログイン済みユーザーがどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in"
         redirect_to login_url
       end
