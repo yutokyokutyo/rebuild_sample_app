430446ae914c765b609914f8bd2f196eaaf3a638
y.kondo <k.yuto.cre@gmail.com>
2017-04-16 01:54:52 +0900

    beforeフィルターでdestroyアクションを管理者だけに限定する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -68,5 +69,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root) unless current_user.admin?
     end
 end
