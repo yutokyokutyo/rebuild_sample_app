d6117564b9c0113884cd35d20e2732c685f813b4
y.kondo <k.yuto.cre@gmail.com>
2017-04-16 01:27:48 +0900

    実際に動作するdestroyアクションを追加する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
   def index
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User.deleted"
+    redirect_to users_url
   end
 
   private
