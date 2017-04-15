2ddacccc7c87dfcb8f760c238ea38d77e91c45d7
y.kondo <k.yuto.cre@gmail.com>
2017-04-16 00:55:20 +0900

    admin属性の変更が禁止されていることをテストする

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,6 +23,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+
+    patch user_path(@other_user), params: {
+                                    user: { password:              "",
+                                            password_confirmation: "",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
   test "should redirect update when not logged in" do
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
