8eb682e1f6eea1fd3d60ebb7a6a69797e2ed8075
y.kondo <k.yuto.cre@gmail.com>
2017-04-15 19:03:57 +0900

    ログイン後のレイアウトテストの追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -17,4 +17,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links when loggin" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    get users_path
+    assert_select "title", full_title("All users")
+  end
 end
