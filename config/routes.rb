cf48a61c6a9442e316e4180b624c9057706b8136
y.kondo <k.yuto.cre@gmail.com>
2017-04-16 15:50:26 +0900

    アカウント有効化に使うリソース (editアクション) を追加す

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
