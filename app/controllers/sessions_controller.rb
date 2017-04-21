d3db54d6014c8d6b460c6b68e020dce5d89c4c41
y.kondo <y.kondo@pepabo.com>
2017-04-21 12:12:58 +0900

    テストで扱うためローカル変数からインスタンス変数に変更
    assings はインスタンス変数にのみアクセスできる。
    しかし @user インスタンス変数を user ローカル変数に書き換えてしまったことで assings がアクセスできなくなってしまった
    
    ERROR["test_login_with_remembering", UsersLoginTest, 1.4665467829981935]
     test_login_with_remembering#UsersLoginTest (1.47s)
    NoMethodError:         NoMethodError: undefined method `remember_token' for nil:NilClass
                test/integration/users_login_test.rb:43:in `block in <class:UsersLoginTest>'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
