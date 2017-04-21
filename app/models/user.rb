1fc81764c67d7fc5e7cffe9e12e708c15b7ac027
y.kondo <y.kondo@pepabo.com>
2017-04-21 12:47:56 +0900

    attributesを使ってDB問い合わせを一度に済ませる

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,8 +44,7 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_attributes(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
