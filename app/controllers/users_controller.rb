cd124a9889b8a8984a6e2b3662450ab8c0ae6b12
y.kondo <y.kondo@pepabo.com>
2017-04-21 14:19:20 +0900

    無効ユーザーは一覧ページを表示させない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    # 有効でないユーザーは表示しない
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
