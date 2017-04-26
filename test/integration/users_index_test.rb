d6e017109412c807fca8ea48e012a7bbf8062fb8
y.kondo <y.kondo@pepabo.com>
2017-04-26 11:02:57 +0900

    実際の動作とテストの動作とで条件を同じにした

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -13,13 +13,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
-      unless user == @invalid_user
-        assert_select 'a[href=?]', user_path(user), text: user.name
-        unless user == @admin
-          assert_select 'a[href=?]', user_path(user), text: 'delete'
-        end
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
     assert_difference 'User.count', -1 do
