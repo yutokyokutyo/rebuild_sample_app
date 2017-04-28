a312a3e3ac601a8aaab22203e325e7ff9a5e29b7
y.kondo <y.kondo@pepabo.com>
2017-04-28 12:51:36 +0900

    パスワード再設定のeditアクション

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -17,4 +20,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
