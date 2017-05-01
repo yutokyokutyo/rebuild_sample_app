449d34a6f00bf70af5ccdeead6d186003907778b
y.kondo <y.kondo@pepabo.com>
2017-05-01 20:35:44 +0900

    SQLのリクエストを減らすためにupdate_columnsを使った

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,8 +55,7 @@ class User < ApplicationRecord
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # パスワード再設定のメールを送信する
