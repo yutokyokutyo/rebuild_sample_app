9e5c76277ec4cd41dcb379e8c9c4e724bcff6687
y.kondo <y.kondo@pepabo.com>
2017-05-04 17:17:21 +0900

    新しいMicropostの有効性に対するテストを追加

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
