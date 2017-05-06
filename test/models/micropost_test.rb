e4a4267e70b15d4f6fac62eff6564d092eb339e9
y.kondo <y.kondo@pepabo.com>
2017-05-06 20:52:35 +0900

    Micropostモデルのバリデーションに対するテストを追加

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -16,4 +16,14 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.user_id = nil
     assert_not @micropost.valid?
   end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
