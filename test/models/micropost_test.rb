6d4d1a9e38b7c73d16639248191d0f032b513103
y.kondo <y.kondo@pepabo.com>
2017-05-08 10:52:43 +0900

    慣習的に正しくマイクロポストを作成する

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,8 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
