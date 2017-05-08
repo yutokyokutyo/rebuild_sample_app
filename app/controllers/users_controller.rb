08dcf9dd65fbb1cc47e9e7c3434e14d04a73c281
y.kondo <y.kondo@pepabo.com>
2017-05-08 12:57:05 +0900

    @micropostsインスタンス変数をshowアクションに追加する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
   end
 
