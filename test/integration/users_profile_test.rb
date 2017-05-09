65a41e65338a7604224a39180dd58764dc3414fc
y.kondo <y.kondo@pepabo.com>
2017-05-09 12:57:09 +0900

    will_paginateが少なくとも１度は表示されていることをテストを追加

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,7 +14,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
-    assert_select 'div.pagination'
+    assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
