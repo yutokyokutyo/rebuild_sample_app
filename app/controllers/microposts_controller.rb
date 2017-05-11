64745439291bf3d31d93e6106286c1758db2129a
y.kondo <y.kondo@pepabo.com>
2017-05-11 16:29:39 +0900

    pictureを許可された属性のリストに追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :picture)
     end
 
     def correct_user
