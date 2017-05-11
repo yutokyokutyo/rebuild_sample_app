8b5fce17d483fbaf6cf80ec8abecd6e9efff1e52
y.kondo <y.kondo@pepabo.com>
2017-05-11 15:20:08 +0900

    rails5系から導入されたredirct_backメソッドに置き換える

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_url
+    redirect_back(fallback_location: root_url)
   end
 
   private
