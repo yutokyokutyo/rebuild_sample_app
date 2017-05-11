121b6433d635b3d0f8c58cfe7d4bd8ef8cf9b622
y.kondo <y.kondo@pepabo.com>
2017-05-11 16:03:30 +0900

    [RED] サイドバーでマイクロポストの投稿数をテストするためのテンプレート

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -33,4 +33,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
   end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{FILL_IN} microposts", response.body
+    # まだマイクロポストを投稿していないユーザー
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match FILL_IN, response.body
+  end
 end
