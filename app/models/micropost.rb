38394b845ff61d421f5033c6a76ef5442e9c5857
y.kondo <y.kondo@pepabo.com>
2017-05-12 14:28:32 +0900

    画像に対するバリデーションを追加する

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
