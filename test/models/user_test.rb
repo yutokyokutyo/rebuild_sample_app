7209165c83c0091007fc43ed255b7351674f4886
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 19:32:30 +0900

    followersに対するテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,6 +91,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
