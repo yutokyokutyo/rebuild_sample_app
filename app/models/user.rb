e80f023eb4a97b8f143445184849c188f29fe61c
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 17:35:09 +0900

    能動的関係に対して１対多(has_many)の関連付けを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
