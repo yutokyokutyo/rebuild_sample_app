c0e074fd2004beac0057b4398c666ed33575d91d
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 18:28:46 +0900

    “following” 関連のメソッドをテストする

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,4 +84,14 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
