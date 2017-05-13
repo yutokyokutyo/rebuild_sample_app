74d0045c325bc3031a03b6d48b9a479b7f6d31f3
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 18:31:29 +0900

    フォロー,アンフォロー,フォロー？メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,6 +80,20 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
   private
 
