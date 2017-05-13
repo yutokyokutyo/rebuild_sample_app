f8cf98ebe67e24949765334b3c6f6b7c2a1a4401
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 18:22:06 +0900

    Userモデルにfollowingの関連付けを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
