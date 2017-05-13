750b09069ce909407dd9e4ab5e072c06cf05c954
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 20:12:42 +0900

    Usersコントローラにfollowingアクションとfollowersアクションを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
