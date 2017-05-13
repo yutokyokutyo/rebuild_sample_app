fc3c2c0481da57a3fdd3e8c3a67ba4dba7838917
y.kondo <k.yuto.cre@gmail.com>
2017-05-13 20:23:36 +0900

    Relationshipリソース用のルーティングを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
