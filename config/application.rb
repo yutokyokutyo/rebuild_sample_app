2eb23918c7ae81d20edcf2d33bba8306dc0a48b0
y.kondo <y.kondo@pepabo.com>
2017-05-15 12:32:20 +0900

    JavaScriptが無効になっていたときのための設定

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RebuildSampleApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
