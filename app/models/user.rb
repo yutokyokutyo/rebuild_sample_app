006183714bc429a93c96ca6a20e05f9170f8dafa
y.kondo <y.kondo@pepabo.com>
2017-05-16 10:38:50 +0900

    whereメソッド内の変数にキーと値のペアを使う

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,7 +80,8 @@ class User < ApplicationRecord
 
   # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+     following_ids: following_ids, user_id: id)
   end
 
   # ユーザーをフォローする
