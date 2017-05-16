b4f19153abca098abb65a7b0ad45ef771c0a2533
y.kondo <y.kondo@pepabo.com>
2017-05-16 16:27:51 +0900

    フィードのHTMLをテストする

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -53,4 +53,11 @@ class FollowingTest < ActionDispatch::IntegrationTest
       delete relationship_path(relationship), xhr: true
     end
   end
+
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
 end
